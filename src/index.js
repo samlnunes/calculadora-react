a329209e8aca122330005cddccc4b628e6b93c9d
Samuel Leite <samuelleiten@gmail.com>
2021-08-29 21:07:00 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
+
+ReactDOM.render(
+  <>
+    <h1>Calculadora</h1> 
+    <Calculator />
+  </>,
+  document.getElementById("root")
+);
